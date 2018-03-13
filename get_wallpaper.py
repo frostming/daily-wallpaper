ef674ccd714d251dad0c011c77958db725665067
frostming <mianghong@gmail.com>
2018-03-13 18:52:05 +0800

    update gitignore
    Signed-off-by: frostming <mianghong@gmail.com>

--- get_wallpaper.py
+++ get_wallpaper.py
@@ -10,13 +10,13 @@ import requests
 import sys
 import os
 
-APP_ID = 'set your application id here'
+APP_KEY = 'APP_KEY'
 BASE_URL = 'https://api.unsplash.com'
 
 
 def get_wallpaper(target_path):
     session = requests.session()
-    params = {'client_id': APP_ID, 'orientation': 'landscape'}
+    params = {'client_id': APP_KEY, 'orientation': 'landscape'}
     resp = session.get(BASE_URL + '/photos/random/', params=params)
     full_url = resp.json()['urls']['full']
     image_resp = session.get(full_url)
